0c2347e61a9e0b2c00ca9992a88dd5cc65701e5f
bantuerfei <34182337+AlexanderAlexLiu@users.noreply.github.com>
2023-01-30 08:13:13 -0500

    first commit of a new branch
    dang it's gonna be a long ride

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PASSGOAT, THE NUMBER GUESSING GAME
+python -m pip install -r requirements.txt
--- run.py
+++
@@ -1,5 +0,0 @@
-from src.game import Game
-
-if __name__ == '__main__':
-    game = Game()
-    game.start()
